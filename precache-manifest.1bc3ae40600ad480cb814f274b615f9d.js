e697d23d68d781ffdfb8222bb2d859fb3dd03283
aparsh <aparsh.gupta@gmail.com>
2021-09-18 15:40:25 +0530

    Updates

---
+++ precache-manifest.1bc3ae40600ad480cb814f274b615f9d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "905dce483ac94f565a1fc6a18c2e4572",
+    "url": "/Restraunt-React/index.html"
+  },
+  {
+    "revision": "edb600ffefbbd6a7543f",
+    "url": "/Restraunt-React/static/css/2.b075f7f5.chunk.css"
+  },
+  {
+    "revision": "3885d0028fa7d1547ab0",
+    "url": "/Restraunt-React/static/css/main.f284b0cb.chunk.css"
+  },
+  {
+    "revision": "edb600ffefbbd6a7543f",
+    "url": "/Restraunt-React/static/js/2.ecf3a3d9.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/Restraunt-React/static/js/2.ecf3a3d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3885d0028fa7d1547ab0",
+    "url": "/Restraunt-React/static/js/main.3c0f83cc.chunk.js"
+  },
+  {
+    "revision": "5f8b37feaa3581795b5b",
+    "url": "/Restraunt-React/static/js/runtime-main.a499265c.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Restraunt-React/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Restraunt-React/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Restraunt-React/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Restraunt-React/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Restraunt-React/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
